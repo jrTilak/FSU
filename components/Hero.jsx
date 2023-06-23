9073c22508b26dc4365067a5a05c06faf4c8b35f
jrTilak <tilakthapa9898@gmail.com>
2023-06-23 16:39:08 +0545

    center desc

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,16 +4,15 @@ const Hero = () => {
     return (
         <>
             <div id="hero-section" className="flex justify-center bg-blue-900 p-5 md:p-16 md:mt-0 md:pt-8  pt-20">
-                <div className="flex flex-col justify-center  max-w-7xl  text-white">
+                <div className="flex flex-col justify-center  max-w-7xl  text-white items-center">
                     <h1 className="text-base font-medium tracking-wider ">Welcome to</h1>
                     <span className="underline underline-offset-2 text-white -mt-3"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                         &nbsp; </span>
-                    <div className="flex flex-col text-white mt-5">
+                    <div className="flex flex-col text-white mt-5 items-center">
                         <h1 className="text-4xl md:text-[50px] font-semibold">स्वतन्त्र विद्यार्थी युनियन,</h1>
                         <p className="text-xl mt-2 md:mt-4 tracking-wide">इ.अ.सं, पूर्वाञ्चल क्याम्पस धरान</p>
                     </div>
-                    <p className="mt-4 text-sm md:w-[52%] tracking-wide leading-7">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit officia pariatur exercitationem vitae iure eaque perspiciatis veniam ea aut, atque est, sit excepturi blanditiis id maiores delectus harum quasi, inventore tenetur consectetur. Earum, labore hic nesciunt inventore corrupti dolor ex ut, blanditiis aperiam distinctio culpa quasi explicabo aliquam! Amet, quasi?</p>
-                    {/* <img class="rounded-full w-96 h-96 absolute right-12" src="https://cdn.ioepc.edu.np/wp-content/uploads/2023/05/ERC_2019_srgb-scaled.jpg" alt="image description" /> */}
+                    <p className="mt-4 text-sm tracking-wide leading-7">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit officia pariatur exercitationem vitae iure eaque perspiciatis veniam ea aut, atque est, sit excepturi blanditiis id maiores delectus harum quasi, inventore tenetur consectetur. Earum, labore hic nesciunt inventore corrupti dolor ex ut, blanditiis aperiam distinctio culpa quasi explicabo aliquam! Amet, quasi?</p>
                     <div className="space-x-3 md:mt-4">
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start ">
                             <Link href="https://www.facebook.com/fsu.ioeerc" className="text-blue-700 bg-white rounded-full p-2 hover:bg-blue-700 hover:text-white">
@@ -31,7 +30,7 @@ const Hero = () => {
                         </span>
 
                     </div>
-                    <div id="hero-btn" className="flex mt-10 space-x-5">
+                    <div id="hero-btn" className="flex mt-10 space-x-5 ">
                         <Link href="#hero-btn">
                             <button className="bg-white text-blue-600 px-6 py-2 hover:brightness-50 font-semibold">Explore</button>
                         </Link>
