9c2be9f12602e07b1dd3d185a233fb68c4376488
jrTilak <tilakthapa9898@gmail.com>
2023-06-23 19:57:31 +0545

    modify post ans news section

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,6 +1,7 @@
 const EventCard = ({title, desc, img, date, time , eventDate, eventTime, daysLeft}) => {
     return (
         <div className="p-4 md:w-1/3">
+            <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 rounded-tl-none rounded-br-none rounded-tr-lg rounder-bl-lg absolute">Upcoming</span>
             <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={img} alt="blog" />
                 <div className="p-6">
