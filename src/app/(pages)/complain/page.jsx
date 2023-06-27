3aed610720cbad11e4253821cb4ab0cda060698b
jrTilak <tilakthapa9898@gmail.com>
2023-06-27 15:30:50 +0545

    fix some bugs in form

--- src/app/(pages)/complain/page.jsx
+++ src/app/(pages)/complain/page.jsx
@@ -10,13 +10,20 @@ const Complain = () => {
     subject: '',
     desc: '',
     else: '',
-    confidential: true
+    confidential: false
   })
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value
+    })
+  }
+
+  const handleConfidential = (e) => {
+    setFormData({
+      ...formData,
+      confidential: formData.confidential ? false : true
     })
   }
 
@@ -38,7 +45,7 @@ const Complain = () => {
             subject: '',
             desc: '',
             else: '',
-            confidential: true
+            confidential: false
           })
         }, function (error) {
           console.log('FAILED...', error);
@@ -50,7 +57,7 @@ const Complain = () => {
   }
   return (
     <section class="text-gray-600 body-font relative mt-16">
-      <div class="container px-5 py-24 mx-auto">
+      <div class="container px-5 py-16 mx-auto">
         <div class="flex flex-col text-center w-full mb-12">
           <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Complain Form</h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-justify">Please use this form to submit a complaint or report any issues you have encountered. We value your feedback and strive to address your concerns promptly and fairly. Thank you for your contribution.</p>
@@ -88,41 +95,24 @@ const Complain = () => {
               </div>
 
               <div class="flex items-center">
-                <input onChange={handleChange} checked={formData.confidential} id="link-checkbox" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 " name='confidential' />
-                <label for="confidential" class="ml-2 text-sm font-medium text-gray-600 ">Mark this complain as <span class="text-blue-600 hover:underline">confidential</span>.</label>
+                <input
+                  onClick={handleConfidential}
+                  id="link-checkbox" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 " name='confidential' />
+                <label for="confidential"
+                  onClick={handleConfidential}
+
+                  class="ml-2 text-sm font-medium text-gray-600 ">Mark this complain as <span class="text-blue-600 hover:underline">confidential</span>.</label>
               </div>
             </div>
             <div class="p-2 w-full">
               <button onClick={handleSubmit} class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
             </div>
             <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-              <a class="text-indigo-500">example@email.com</a>
-              <p class="leading-normal my-5">49 Smith St.
-                <br />Saint Cloud, MN 56301
+              <a href='mailto:fsu.erc@ioepc.edu.np' class="text-indigo-500">fsu.erc@ioepc.edu.np</a>
+              <p class="leading-normal my-5">Gangalal Marg, Tinkune,
+                <br />Dharan-8, Sunsari
               </p>
-              <span class="inline-flex">
-                <a class="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a class="ml-4 text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a class="ml-4 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                  </svg>
-                </a>
-                <a class="ml-4 text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
+
             </div>
           </div>
         </div>
