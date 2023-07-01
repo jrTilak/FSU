9f57a296e3ee65ef092eedb3193873baedaf7077
jr Tilak <tilakthapa9898@gmail.com>
2023-07-01 09:14:18 +0545

    Update getEventStatus.js

--- src/utils/getEventStatus.js
+++ src/utils/getEventStatus.js
@@ -1,30 +1,32 @@
-const getEventStatus = (event_date, event_time) => {
-    // Convert date and time strings to Date objects
-    const currentDatetime = new Date();
-    const eventDatetime = new Date(`${event_date} ${event_time}`);
+const getEventStatus = (event_date) => {
+  // Convert date strings to Date objects
+  const currentDate = new Date();
+  const eventDate = new Date(event_date);
 
-    // Calculate the number of days left until the event
-    const timeDiff = eventDatetime.getTime() - currentDatetime.getTime();
-    const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  // Set the time of both dates to 0:00:00
+  currentDate.setHours(0, 0, 0, 0);
+  eventDate.setHours(0, 0, 0, 0);
 
+  // Calculate the number of days left until the event
+  const timeDiff = eventDate.getTime() - currentDate.getTime();
+  const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-    if (daysLeft === 0) {
-        return {
-            status: "today",
-            daysLeft: null
-        };
-    }
-    else if (currentDatetime > eventDatetime) {
-        return {
-            status: "completed",
-            daysLeft: null
-        };
-    }
-
+  if (daysLeft === 0) {
     return {
-        status: "upcoming",
-        daysLeft: daysLeft
+      status: "today",
+      daysLeft: null
     };
+  } else if (currentDate > eventDate) {
+    return {
+      status: "completed",
+      daysLeft: null
+    };
+  }
+
+  return {
+    status: "upcoming",
+    daysLeft: daysLeft
+  };
 };
 
 export default getEventStatus;
